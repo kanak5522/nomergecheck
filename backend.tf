fa7e5162abfbc1d501d29ec85a5e94a84d06b711
kanak5522 <kanakchandel552000@gmail.com>
2024-09-05 23:43:14 -0700

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,10 @@
+
+ terraform {
+   backend "s3" {
+     bucket         = "nobuckk"
+     key            = "terraform/state.tfstate"
+     region         = "eu-west-1"
+     dynamodb_table = "noolock"
+     encrypt        = true
+   }
+ }
